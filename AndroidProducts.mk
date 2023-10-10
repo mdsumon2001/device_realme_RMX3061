5ce9a55d1c3821c09048ef222f36f1b4f53b5fd9
sumon <mdsumonali751@gmail.com>
2023-10-10 07:02:06 +0600

    RMX-3061: Fixed Build type and lunch choice.
    Signed-off-by: sumon <mdsumonali751@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,13 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/rr_RMX3061.mk
-   
+
 COMMON_LUNCH_CHOICES := \
-    rr_RMX3061-userdebug
-    rr_RMX3061-user
+    rr_RMX3061-user \
+    rr_RMX3061-userdebug \
     rr_RMX3061-eng
